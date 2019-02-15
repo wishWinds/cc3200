8054e2581405622c247ffcbb86b8dcdd86f49578
shupeng <john.hi.gm@gmail.com>
2019-02-15 09:50:27 +0800

    constant response

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/config', function(req, res, next) {
+  res.json({
+    "dev_id": req.query.dev_id,
+    "timestamp": Math.floor(Date.now() / 1000),
+    "sample_param": {
+      "trigger_mechanism":"event",
+      "trigger_value":200,
+      "sample_time":30,
+      "sample_freq":200
+    }
+  })
+})
+
 module.exports = router;
