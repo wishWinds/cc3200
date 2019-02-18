d0a4e9dc2477b97a28656d0f0fa04452d206cd00
shupeng <john.hi.gm@gmail.com>
2019-02-19 00:51:31 +0800

    add xmlhttp request

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+var configs = {}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname + '/../index.html'))
 });
 
-router.get('/config', function(req, res, next) {
+router.get('/config', function (req, res, next) {
+  var dev_id = req.query.dev_id
+  if (dev_id) {
+    var data = configs[dev_id]
+    if (data) {
+      config = data["config"]
+      configs[dev_id]["hasFetch"] = true
+      res.json({
+        "dev_id": config["dev_id"],
+        "timestamp": Math.floor(Date.now() / 1000),
+        "sample_param": {
+          "trigger_mechanism": config["trigger_mechanism"],
+          "trigger_value": config["trigger_value"],
+          "sample_time": config["sample_time"],
+          "sample_freq": config["sample_freq"],
+          "sample_intvl": config["sample_intvl"]
+        }
+      })
+    }
+  }
+
   res.json({
-    "dev_id": req.query.dev_id,
+    "dev_id": dev_id,
     "timestamp": Math.floor(Date.now() / 1000),
     "sample_param": {
-      "trigger_mechanism":"event",
-      "trigger_value":200,
-      "sample_time":30,
-      "sample_freq":200
     }
   })
+
 })
 
+router.post('/config', function (req, res, next) {
+  var config = req.query
+  configs[config["dev_id"]] = {
+    "config": config,
+    "hasFetch": false
+  }
+  res.json({ ret: true })
+})
 module.exports = router;
