977033877810e98e29d60d60fe89d4cee13a2692
shupeng <john.hi.gm@gmail.com>
2019-02-19 10:39:58 +0800

    upload files

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var uploadRouter = require('./routes/upload');
 var usersRouter = require('./routes/users');
+
+var serveIndex = require('serve-index');
 
 var app = express();
 
@@ -20,6 +23,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/upload', uploadRouter);
+app.use('/uploadFiles', express.static('uploadFiles'), serveIndex('uploadFiles', {'icon': true}));
+app.use('/uploadFilesTest', express.static('uploadFilesTest'), serveIndex('uploadFilesTest', {'icon': true}));
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
---
+++ routes/upload.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        var filePath = path.join(__dirname, '../uploadFiles/' + file.fieldname);
+        if (!fs.existsSync(filePath)) {
+            fs.mkdirSync(filePath);
+        }
+        cb(null, filePath);
+    },
+    filename: function (req, file, cb) {
+        var timestamp = Date.now();
+        cb(null, timestamp + '-' + Date(timestamp).toLocaleString() + '.txt')
+    }
+})
+
+var storageTest = multer.diskStorage({
+    destination: function (req, file, cb) {
+        var filePath = path.join(__dirname, '../uploadFilesTest/' + file.fieldname);
+        if (!fs.existsSync(filePath)) {
+            fs.mkdirSync(filePath);
+        }
+        cb(null, filePath);
+    },
+    filename: function (req, file, cb) {
+        var timestamp = Date.now();
+        cb(null, timestamp + '-' + Date(timestamp).toLocaleString() + '.txt')
+    }
+})
+
+var upload = multer({ storage: storage});
+var uploadTest = multer({ storage: storageTest});
+
+/* GET users listing. */
+router.post('/', upload.any(), function (req, res, next) {
+
+    if (req.files.length > 0) {
+        res.json({err: 0, message: 'upload success!'})
+    }
+    else {
+        res.json({err: -1, message: 'no csv file'})
+    }
+});
+
+router.post('/test', uploadTest.any(), function (req, res, next) {
+
+    if (req.files.length > 0) {
+        res.json({err: 0, message: 'upload success!'})
+    }
+    else {
+        res.json({err: -1, message: 'no csv file'})
+    }
+});
+
+module.exports = router;
