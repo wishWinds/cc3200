432c6dbd2e21e56dbb2eab1df38426060500a1cf
shupeng <john.hi.gm@gmail.com>
2019-02-27 13:01:09 +0800

    update log to common

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger('common'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
