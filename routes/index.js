12b10023cc8d0e3e511f078af093ec8888754329
shupeng <john.hi.gm@gmail.com>
2019-04-18 10:29:01 +0800

    add clear function

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,24 @@ var semver = require('semver');
 
 var binAddressBase = "http://xyz.fancyjohn.com:60001/dev_bin/"
 
+var deleteFolderRecursive = function(path) {
+    if (fs.existsSync(path)) {
+        fs.readdirSync(path).forEach(function(file, index) {
+            var curPath = path + "/" + file;
+            if (fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.sendFile(path.join(__dirname + '/../index.html'))
+router.get('/', function(req, res, next) {
+    // res.render('index', { title: 'Express' });
+    res.sendFile(path.join(__dirname + '/../index.html'))
 });
 
 
@@ -17,110 +31,117 @@ router.get('/', function (req, res, next) {
 var configs = fs.readFileSync(path.join(__dirname + "/../configs.json"));
 configs = JSON.parse(configs);
 
-router.get('/config', function (req, res, next) {
-  var pre = fs.readFileSync(path.join(__dirname + "/../preference.json"));
-  pre = JSON.parse(pre);
+router.post('/remove', function(req, res, next) {
+    var dir = path.join(__dirname + "/../uploadFilesTest");
+    deleteFolderRecursive(dir);
+    fs.mkdirSync(dir);
 
-  var dev_id = req.query.dev_id
-  var version = req.query.soft_ver;
-  if (dev_id) {
-    var data = configs[dev_id]
-    if (data) {
-      config = data["config"]
-      configs[dev_id]["hasFetch"] = true;
-      saveConfigs();
-      var ret = {
-        "dev_id": config["dev_id"],
-        "timestamp": Math.floor(Date.now() / 1000),
-        "sample_param": {
-          "trigger_mechanism": config["trigger_mechanism"],
-          "trigger_value": parseInt(config["trigger_value"]) ? parseInt(config["trigger_value"]) : 0,
-          "sample_time": parseInt(config["sample_time"]) ? parseInt(config["sample_time"]) : 0,
-          "sample_freq": parseInt(config["sample_freq"]) ? parseInt(config["sample_freq"]) : 0,
-          "sample_intvl": parseInt(config["sample_intvl"]) ? parseInt(config["sample_intvl"]) : 0,
+    res.send("clear success");
+})
+
+router.get('/config', function(req, res, next) {
+    var pre = fs.readFileSync(path.join(__dirname + "/../preference.json"));
+    pre = JSON.parse(pre);
+
+    var dev_id = req.query.dev_id
+    var version = req.query.soft_ver;
+    if (dev_id) {
+        var data = configs[dev_id]
+        if (data) {
+            config = data["config"]
+            configs[dev_id]["hasFetch"] = true;
+            saveConfigs();
+            var ret = {
+                "dev_id": config["dev_id"],
+                "timestamp": Math.floor(Date.now() / 1000),
+                "sample_param": {
+                    "trigger_mechanism": config["trigger_mechanism"],
+                    "trigger_value": parseInt(config["trigger_value"]) ? parseInt(config["trigger_value"]) : 0,
+                    "sample_time": parseInt(config["sample_time"]) ? parseInt(config["sample_time"]) : 0,
+                    "sample_freq": parseInt(config["sample_freq"]) ? parseInt(config["sample_freq"]) : 0,
+                    "sample_intvl": parseInt(config["sample_intvl"]) ? parseInt(config["sample_intvl"]) : 0,
+                }
+            }
+            if (pre.upgrade && shouldUpgrade(version)) {
+                ret["sample_param"]["ota"] = { "url": binAddressBase + pre.lastBinName }
+            }
+            res.json(ret)
+            return
         }
-      }
-      if (pre.upgrade && shouldUpgrade(version)) {
+    }
+
+    var ret = {
+        "dev_id": dev_id,
+        "timestamp": Math.floor(Date.now() / 1000),
+        "sample_param": {}
+    }
+
+    if (pre.upgrade && shouldUpgrade(version)) {
         ret["sample_param"]["ota"] = { "url": binAddressBase + pre.lastBinName }
-      }
-      res.json(ret)
-      return
     }
-  }
 
-  var ret = {
-    "dev_id": dev_id,
-    "timestamp": Math.floor(Date.now() / 1000),
-    "sample_param": {
+    res.json(ret)
+
+})
+
+router.post('/config', function(req, res, next) {
+    var config = req.body
+    configs[config["dev_id"]] = {
+        "config": config,
+        "hasFetch": false
     }
-  }
-
-  if (pre.upgrade && shouldUpgrade(version)) {
-    ret["sample_param"]["ota"] = { "url": binAddressBase + pre.lastBinName }
-  }
-
-  res.json(ret)
-
+    saveConfigs();
+    res.json({ ret: true })
 })
 
-router.post('/config', function (req, res, next) {
-  var config = req.body
-  configs[config["dev_id"]] = {
-    "config": config,
-    "hasFetch": false
-  }
-  saveConfigs();
-  res.json({ ret: true })
+router.get('/upgrade', function(req, res, next) {
+    var pre = fs.readFileSync(path.join(__dirname + "/../preference.json"));
+    pre = JSON.parse(pre);
+    res.json({ "upgrade": pre.upgrade })
 })
 
-router.get('/upgrade', function (req, res, next) {
-  var pre = fs.readFileSync(path.join(__dirname + "/../preference.json"));
-  pre = JSON.parse(pre);
-  res.json({ "upgrade": pre.upgrade })
+router.post('/upgrade', function(req, res, next) {
+    var pre = fs.readFileSync(path.join(__dirname + "/../preference.json"));
+    pre = JSON.parse(pre);
+    pre.upgrade = req.body["upgrade"]
+    savePreference(pre);
+    res.json({ ret: true })
 })
 
-router.post('/upgrade', function (req, res, next) {
-  var pre = fs.readFileSync(path.join(__dirname + "/../preference.json"));
-  pre = JSON.parse(pre);
-  pre.upgrade = req.body["upgrade"]
-  savePreference(pre);
-  res.json({ ret: true })
-})
-
-router.get('/lastVersion', function (req, res, next) {
-  var pre = fs.readFileSync(path.join(__dirname + "/../preference.json"));
-  pre = JSON.parse(pre);
-  res.json({ latestVersion: getBinNameVersion()})
+router.get('/lastVersion', function(req, res, next) {
+    var pre = fs.readFileSync(path.join(__dirname + "/../preference.json"));
+    pre = JSON.parse(pre);
+    res.json({ latestVersion: getBinNameVersion() })
 })
 
 function saveConfigs() {
-  fs.writeFileSync(path.join(__dirname + "/../configs.json"), JSON.stringify(configs, null, 2));
+    fs.writeFileSync(path.join(__dirname + "/../configs.json"), JSON.stringify(configs, null, 2));
 }
 
 function savePreference(pre) {
-  fs.writeFileSync(path.join(__dirname + "/../preference.json"), JSON.stringify(pre, null, 2));
+    fs.writeFileSync(path.join(__dirname + "/../preference.json"), JSON.stringify(pre, null, 2));
 }
 
 function getBinNameVersion() {
-  var pre = fs.readFileSync(path.join(__dirname + "/../preference.json"));
-  pre = JSON.parse(pre);
-  var binName = pre.lastBinName;
-  var version = binName.slice(binName.indexOf("_V") + 2, binName.indexOf(".bin"));
-  return version;
+    var pre = fs.readFileSync(path.join(__dirname + "/../preference.json"));
+    pre = JSON.parse(pre);
+    var binName = pre.lastBinName;
+    var version = binName.slice(binName.indexOf("_V") + 2, binName.indexOf(".bin"));
+    return version;
 }
 
 function shouldUpgrade(currVer) {
-  var binVersion = getBinNameVersion();
-  // if (semver.gte(binVersion, currVer)) {
-  //   return true;
-  // } else {
-  //   return false;
-  // }
-  if (binVersion == currVer) {
-    return false;
-  } else {
-    return true;
-  }
+    var binVersion = getBinNameVersion();
+    // if (semver.gte(binVersion, currVer)) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    if (binVersion == currVer) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 module.exports = router;
