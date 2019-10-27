071d2ab8eb3e5049c4da4c5eeb02862811057c9c
shupeng <john.hi.gm@gmail.com>
2019-10-27 10:21:26 +0800

    remote test

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/upload', uploadRouter);
 app.use('/uploadFiles', express.static('uploadFiles'), serveIndex('uploadFiles', {'icon': true}));
-app.use('/uploadFilesTest', express.static('uploadFilesTest'), serveIndex('uploadFilesTest', {'icon': true}));
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
